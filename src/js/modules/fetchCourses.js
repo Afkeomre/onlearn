649d25a1ae33cafe54673379bf34a5d64166e1f3
Afkeomre <voronk0va.ir@yandex.ru>
2024-06-20 11:58:37 +0300

    fix tabs work

--- src/js/modules/fetchCourses.js
+++ src/js/modules/fetchCourses.js
@@ -40,34 +40,3 @@ const fetchCourses = () => {
 };
 
 export default fetchCourses;
-
-// data.forEach((arr) => {
-// 	parents.forEach((parent) => {
-// 		arr.forEach((item) => {
-// 			parent.insertAdjacentHTML(
-// 				'beforeend',
-// 				`<article class="courses__card">
-// 						<div class="courses__card-info">
-// 							<img class="courses__card-img" src="./../../../img/courses/${item.img}.jpg" alt="Course">
-// 							<div class="courses__card-statistic">
-// 								<img src="${item.avatars}" alt="Avatars">
-// 								<span class="courses__card-number"><span class="plus">+</span> ${item.number} students</span>
-// 							</div>
-// 						</div>
-// 						<div class="courses__card-content">
-// 						<div class="courses__card-date">${item.date}</div>
-// 						<h4 class="courses__card-title">${item.title}</h4>
-// 						<p class="courses__card-description">${item.description}</p>
-// 						<div class="courses__card-footer">
-// 							<div class="courses__card-price">
-// 								<span class="courses__card-price_new">$ ${item.newPrice} </span>
-// 								<span class="courses__card-price_old">$ ${item.oldPrice}</span>
-// 							</div>
-// 							<button class="btn btn_small btn_cyan">Enroll now</button>
-// 							</div>
-// 						</div>
-// 					</article>`
-// 			);
-// 		});
-// 	});
-// });
